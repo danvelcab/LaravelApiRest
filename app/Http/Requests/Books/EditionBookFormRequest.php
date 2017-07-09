58bb6ff39cacbc198bb09bd1c1021d4ffdfe62cf
Daniel <danvelcab@gmail.com>
2017-07-09 11:54:11 +0200

    Fixed bug. Rule "number" -> "numeric"

--- app/Http/Requests/Books/EditionBookFormRequest.php
+++ app/Http/Requests/Books/EditionBookFormRequest.php
@@ -27,7 +27,7 @@ class EditionBookFormRequest extends FormRequest
     {
         $rules = [
             "title"                  =>      "required|string|min:1|max:100",
-            "price"                  =>      "required|number|min:0|max:100",
+            "price"                  =>      "required|numeric|min:0|max:100",
             "author"                 =>      "required|string|min:1|max:100",
             "editor"                 =>      "required|string|min:1|max:100"
         ];
